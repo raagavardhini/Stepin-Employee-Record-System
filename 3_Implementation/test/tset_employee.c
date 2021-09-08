fb19ddd1235ad75fe746f71d8bc148310d7ba99e
raagavardhini <raagavardhini12@gmail.com>
2021-09-08 10:25:02 +0530

    update

---
+++ 3_Implementation/test/tset_employee.c
@@ -0,0 +1,59 @@
+/**
+ * @file tset_employee.c
+ * @author raaga.vardhini@ltts.com
+ * @brief 
+ * @version 0.1
+ * @date 2021-09-08
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+#include "unity.h"
+#include <main.h>
+
+/* Modify these two lines according to the project */
+#include <main.h>
+#define PROJECT_NAME    "Employee-Record-System"
+
+/* Prototypes for all the test functions */
+void test_Add_Record(void);
+void test_List_Record(void);
+void test_Modify_Record(void);
+void test_Delete_Record(void);
+void test_Exit(void);
+
+/* Required by the unity test framework */
+void setUp(){}
+/* Required by the unity test framework */
+void tearDown(){}
+
+/* Start of the application test */
+int main()
+{
+/* Initiate the Unity Test Framework */
+  UNITY_BEGIN();
+
+/* Run Test functions */
+  RUN_TEST(test_Add_Record);
+  RUN_TEST(test_Modify_Record);
+  RUN_TEST(test_Exit);
+
+  /* Close the Unity Test Framework */
+  return UNITY_END();
+}
+
+/* Write all the test functions */ 
+void test_ADD_Record(void) {
+  TEST_ASSERT_EQUAL(raaga 22 35000, Add_Record(raaga, 15, 35000));
+  
+  /* Dummy fail*/
+  TEST_ASSERT_EQUAL(12 raaga 35000, Add_Record(raaga, 12, 7500));
+}
+
+void test_Modify_Record(void) {
+  TEST_ASSERT_EQUAL(raaga, Modify_Record( raaga));
+  
+  /* Dummy fail*/
+  TEST_ASSERT_EQUAL(raaga, Modify_Record(abc));
+}
+
